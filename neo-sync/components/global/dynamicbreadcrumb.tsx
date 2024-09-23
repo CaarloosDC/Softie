80d0f253128c27d0365a23148f14473c668815ab
Carlos <A00836623@tec.mx>
2024-09-22 23:39:20 -0600

    Cambios en el breadcrumb. Prisma ORM sustituido por supabase client

--- neo-sync/components/global/dynamicbreadcrumb.tsx
+++ neo-sync/components/global/dynamicbreadcrumb.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import React, { ReactNode } from "react"
-import { usePathname } from "next/navigation"
-import { ChevronRight, Home } from "lucide-react"
+import React, { ReactNode } from "react";
+import { usePathname } from "next/navigation";
+import { ChevronRight, Home } from "lucide-react";
 
 import {
   Breadcrumb,
@@ -11,37 +11,39 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
+} from "@/components/ui/breadcrumb";
 
 type TBreadCrumbProps = {
-  homeElement?: ReactNode
-  separator?: ReactNode
-  capitalizeLinks?: boolean
-}
+  homeElement?: ReactNode;
+  separator?: ReactNode;
+  capitalizeLinks?: boolean;
+};
 
 export default function DynamicBreadcrumb({
   homeElement = <Home className="h-4 w-4" />,
   separator = <ChevronRight className="h-4 w-4" />,
   capitalizeLinks = false,
 }: TBreadCrumbProps) {
-  const paths = usePathname()
-  const pathNames = paths.split("/").filter((path) => path)
+  const paths = usePathname();
+  const pathNames = paths.split("/").filter((path) => path);
 
   return (
     <Breadcrumb>
       <BreadcrumbList>
         <BreadcrumbItem>
           <BreadcrumbLink href="/" className="flex items-center">
-            {homeElement}
+            {homeElement}Home
           </BreadcrumbLink>
         </BreadcrumbItem>
-        {pathNames.length > 0 && <BreadcrumbSeparator>{separator}</BreadcrumbSeparator>}
+        {pathNames.length > 0 && (
+          <BreadcrumbSeparator>{separator}</BreadcrumbSeparator>
+        )}
         {pathNames.map((link, index) => {
-          const href = `/${pathNames.slice(0, index + 1).join("/")}`
-          const isLast = index === pathNames.length - 1
+          const href = `/${pathNames.slice(0, index + 1).join("/")}`;
+          const isLast = index === pathNames.length - 1;
           const itemLink = capitalizeLinks
             ? link[0].toUpperCase() + link.slice(1)
-            : link
+            : link;
 
           return (
             <React.Fragment key={index}>
@@ -52,11 +54,13 @@ export default function DynamicBreadcrumb({
                   <BreadcrumbLink href={href}>{itemLink}</BreadcrumbLink>
                 )}
               </BreadcrumbItem>
-              {!isLast && <BreadcrumbSeparator>{separator}</BreadcrumbSeparator>}
+              {!isLast && (
+                <BreadcrumbSeparator>{separator}</BreadcrumbSeparator>
+              )}
             </React.Fragment>
-          )
+          );
         })}
       </BreadcrumbList>
     </Breadcrumb>
-  )
+  );
 }
