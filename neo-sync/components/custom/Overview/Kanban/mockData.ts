838a8fac1fef80e6f1e109107f131b096ec6d6d2
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-09-25 13:15:22 -0600

    Add comments.

---
+++ neo-sync/components/custom/Overview/Kanban/mockData.ts
@@ -0,0 +1,82 @@
+import { type Task, TaskCard } from "./TaskCard";
+
+export const initialTasks: Task[] = [
+  {
+    id: "task1",
+    columnId: "done",
+    title: "Project Kickoff",
+    content: "Project initiation and planning",
+  },
+  {
+    id: "task2",
+    columnId: "done",
+    title: "Requirement Gathering",
+    content: "Gather requirements from stakeholders",
+  },
+  {
+    id: "task3",
+    columnId: "done",
+    title: "Wireframes and Mockups",
+    content: "Create wireframes and mockups",
+  },
+  {
+    id: "task4",
+    columnId: "in-progress",
+    title: "Homepage Development",
+    content: "Develop homepage layout",
+  },
+  {
+    id: "task5",
+    columnId: "in-progress",
+    title: "UI Design",
+    content: "Design color scheme and typography",
+  },
+  {
+    id: "task6",
+    columnId: "todo",
+    title: "User Authentication",
+    content: "Implement user authentication",
+  },
+  {
+    id: "task7",
+    columnId: "todo",
+    title: "Contact Page",
+    content: "Build contact us page",
+  },
+  {
+    id: "task8",
+    columnId: "todo",
+    title: "Product Catalog",
+    content: "Create product catalog",
+  },
+  {
+    id: "task9",
+    columnId: "todo",
+    title: "About Us",
+    content: "Develop about us page",
+  },
+  {
+    id: "task10",
+    columnId: "todo",
+    title: "Mobile Optimization",
+    content: "Optimize website for mobile devices",
+  },
+  {
+    id: "task11",
+    columnId: "todo",
+    title: "Payment Integration",
+    content: "Integrate payment gateway",
+  },
+  {
+    id: "task12",
+    columnId: "todo",
+    title: "Testing and Bug Fixes",
+    content: "Perform testing and bug fixing",
+  },
+  {
+    id: "task13",
+    columnId: "todo",
+    title: "Launch and Deployment",
+    content: "Launch website and deploy to server",
+  },
+];
