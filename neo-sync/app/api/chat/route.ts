e6d6a0f03989c5d837370491b4c77b82ca8007f4
Sebastian Rosas <rosassebastian2003@gmail.com>
2024-10-31 16:36:27 -0600

    finished context grabbing

--- neo-sync/app/api/chat/route.ts
+++ neo-sync/app/api/chat/route.ts
@@ -2,6 +2,7 @@ import { StreamingTextResponse, Message } from "ai";
 import { AIMessage, HumanMessage, SystemMessage } from "langchain/schema";
 import { ChatOllama } from "langchain/chat_models/ollama";
 import { BytesOutputParser } from "langchain/schema/output_parser";
+import { retrieveRelevantDocs } from "@/lib/ollama/retrieval/retrieveRelevantDocs";
 
 export const runtime = "edge";
 
@@ -15,9 +16,31 @@ export async function POST(req: Request) {
 
   const parser = new BytesOutputParser();
 
+  // Obtener el ultimo mensaje del usuario
+  const userMessage = messages.find((m: Message) => m.role === "user");
+
+  // Obtener el contexto de la empresa
+  const neorisDocs = await retrieveRelevantDocs("Neoris");
+  const neorisContext = neorisDocs.map((doc: { content: any; }) => doc.content).join('\n');
+
+  // Obtener contexto relevante a la solicitud del usuario
+  const userDocs = await retrieveRelevantDocs(userMessage.content);
+  const userContext = userDocs.map((doc: { content: any; }) => doc.content).join('\n');
+
   // Agregar el mensaje de sistema
   const systemMessage = new SystemMessage(
-    "Tu nombre es Softie, eres un asistente de inteligencia artificial que busca asistir al usuario con dudas respecto a la generacion de requerimientos funcionales y no funcionales para proyectos de software, asi como otros temas relacionados con dicha materia"
+    `Tu nombre es Softie, eres un asistente de inteligencia artificial que busca asistir al usuario con dudas respecto a la generacion de requerimientos funcionales y no funcionales para proyectos de software, asi como otros temas relacionados con dicha materia.
+
+    Tu trabajas para la empresa Neoris. Neoris es una empresa global de consultoría en tecnologías de la información y negocios, que ofrece servicios de transformación digital, desarrollo de software, y soluciones tecnológicas innovadoras.
+
+    Aquí hay un poco de contexto sobre Neoris:
+    ${neorisContext}
+
+    Aquí hay un poco de información relevante a la solicitud hecha por el usuario:
+    ${userContext}
+
+    Si el usuario pregunta "¿Qué es Neoris?", o algo similar donde solicite saber algo sobre Neoris, responde con: "Neoris es una empresa global de consultoría en tecnologías de la información y negocios, que ofrece servicios de transformación digital, desarrollo de software, y soluciones tecnológicas innovadoras."
+    `
   );
 
   // Incluir el mensaje de sistema en el array de mensajes
