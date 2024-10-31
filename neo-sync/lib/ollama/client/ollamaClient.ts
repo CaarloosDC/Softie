23a2c33eee5266e9769e9f497bf412814edd9890
Sebastian Rosas <rosassebastian2003@gmail.com>
2024-10-31 15:52:06 -0600

    Merged UI fixes

--- neo-sync/lib/ollama/client/ollamaClient.ts
+++ neo-sync/lib/ollama/client/ollamaClient.ts
@@ -2,4 +2,4 @@ import { Ollama } from 'ollama'
 
 export const ollamaHost = process.env.OLLAMA_BASE_URL
 
-export const ollamaClient = new Ollama({ host: 'http://127.0.0.1:11434' })
+export const ollamaClient = new Ollama({ host: ollamaHost })
--- neo-sync/lib/ollama/client/ollamaEmbeddingClient.ts
+++ neo-sync/lib/ollama/client/ollamaEmbeddingClient.ts
@@ -1,6 +1,8 @@
 import { OllamaEmbeddings } from "@langchain/ollama";
 
+const baseUrl = process.env.OLLAMA_BASE_URL
+
 export const ollamaEmbeddingClient = new OllamaEmbeddings({
     model: "jmorgan/gte-small", // Default value
-    baseUrl: "http://localhost:11434", // Default value
+    baseUrl: baseUrl, // Default value
 });
