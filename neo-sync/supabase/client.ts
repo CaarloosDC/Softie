cdf6809ea16b4bd49ecae5f92d08f2dd4ddc13a0
Sebastian Rosas <rosassebastian2003@gmail.com>
2024-11-12 16:41:32 -0600

    no message

--- neo-sync/supabase/client.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@/utils/supabase/client";
-
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_SERVICE_KEY;
-
-export const supabaseClient = createClient();
