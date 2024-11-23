8889647e1c8ee9f7dd098415cdb00e788a4bb6c3
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-11-22 18:22:16 -0600

    Change login image.

--- neo-sync/components/custom/login-image.tsx
+++ neo-sync/components/custom/login-image.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function LoginImage() {
   return (
+    <div className="hidden lg:block bg-[#18181b] h-full w-full max-h-screen" />
+  );
+
+  return (
     <div className="hidden bg-muted lg:block">
       <Image
         src="/login/1.webp"
